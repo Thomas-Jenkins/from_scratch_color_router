bcf21fe3f16e5624bef12f97d14a5a5db6baf0b8
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-09-27 10:08:37 -0700

    add basic wireframe of webpage using components

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import Display from './Display/Display';
+import Nav from './Nav/Nav';
+
+
+export default function Main() {
+  return (
+    <>
+      <Nav />
+      <Display />
+    </>
+  );
+}
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import './Nav.css';
+export default function Nav() {
+  return (
+    <div className="main">
+      <div>
+        Color 1
+      </div>
+      <div>
+        Color 2
+      </div>
+      <div>
+        Color 3
+      </div>
+    </div>
+  );
+}
